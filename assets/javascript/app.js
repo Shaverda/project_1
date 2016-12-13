40d8a36bae3b6a47dc7bba1abae29743c01c1fd3
Shaverda <shaverda@gmail.com>
2016-12-12 21:21:10 -0600

    initial file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+//$(document).ready(){
+
+var FlightRequest = {
+  "request": {
+    "slice": [
+      {
+        "origin": "DCA",
+        "destination": "LAX",
+        "date": "2017-01-25"
+      }
+    ],
+    "passengers": {
+      "adultCount": 1,
+    },
+
+    "solutions": 2,
+    "refundable": false
+  }
+};
+
+$.ajax({
+     type: "POST",
+     //Set up your request URL and API Key.
+     url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBPM6wdALkjvVZGjgS0ziYqkfBjB1CzZMo", 
+     contentType: 'application/json', // Set Content-type: application/json
+     dataType: 'json',
+     // The query we want from Google QPX, This will be the variable we created in the beginning
+     data: JSON.stringify(FlightRequest),
+     success: function (data) {
+      //Once we get the result you can either send it to console or use it anywhere you like.
+      console.log(JSON.stringify(data));
+    },
+      error: function(){
+       //Error Handling for our request
+       alert("Access to Google QPX Failed.");
+     }
+    });
+
+
+
+//}
