486bf3074df97a4ff69167ce8c48d57251782947
Shaverda <shaverda@gmail.com>
2016-12-17 11:27:19 -0600

    confused

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,80 +4,39 @@ $( function() {
   });
 } );
 
-var destination_options = 
-  [["Atlanta, GA", "ATL"],
-  ["Chicago, IL " , "ORD"],
-  ["Los Angeles, CA" , "LAX"],
-  ["Denver, CO"   , "DEN"],
-  ["New York, NY" , "JFK"],
-  ["San Francisco, CA" , "SFO"],
-  ["Charlotte, NC" , "CLT"]];
-
-var flight_request = {
-  "request": {
-    "slice": [
-      {
-        "origin": "AUS",
-        "destination": "LAX",
-        "date": "2017-01-25"
-      }
-    ],
-    "passengers": {
-      "adultCount": 1,
-    },
-    "solutions": 2,
-    "refundable": false
-  }
+var config = {
+  apiKey: "AIzaSyBPM6wdALkjvVZGjgS0ziYqkfBjB1CzZMo",
+  authDomain: "jetsetters-866cf.firebaseapp.com",
+  databaseURL: "https://jetsetters-866cf.firebaseio.com",
+  storageBucket: "jetsetters-866cf.appspot.com",
+  messagingSenderId: "26725366808"
 };
+firebase.initializeApp(config);
 
-var starting_airport; var departure_date;
+var database = firebase.database();
+var flight_options_ref = database.ref("flight_options");
 
 $("#submit").on("click", function(event){
-
-  starting_airport = document.getElementById('select_start_airport').value;
-
-  // console.log(starting_airport);
-
-  var destination_number = Math.floor(Math.random() * (destination_options.length));
-
-  departure_date = $("#datepicker").val();
+  event.preventDefault();
+  var name = $("#name-area").val().trim();
+  var email = $("#email-area").val().trim();
+  var airport_code = document.getElementById('select_start_airport').value;
+  var departure_date = $("#datepicker").val();
   departure_date = moment(departure_date).format('YYYY-MM-DD');
 
-  flight_request.request.slice[0].origin = starting_airport;
-  flight_request.request.slice[0].date = departure_date;
-  flight_request.request.slice[0].destination = destination_options[destination_number][1];
+  console.log(airport_code);
 
-
-  $.ajax({
-     type: "POST",
-     //Set up your request URL and API Key.
-     url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBPM6wdALkjvVZGjgS0ziYqkfBjB1CzZMo", 
-     contentType: 'application/json', // Set Content-type: application/json
-     dataType: 'json',
-     // The query we want from Google QPX, This will be the variable we created in the beginning
-     data: JSON.stringify(flight_request),
-     success: function (data) {
-      //Once we get the result you can either send it to console or use it anywhere you like.
-      console.log(JSON.stringify(data));
-    },
-      error: function(){
-       //Error Handling for our request
-       alert("Access to Google QPX Failed.");
-     }
+  flight_options_ref.set({
+    firstName : name,
+    emailAddress : email,
+    airport : airport_code,
+    date: departure_date
   });
 
 });
 
 
-// function initMap() {
-//   var austin = {lat: 30.307182, lng: -97.755996};
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 5,
-//     center: austin
-//   });
-//   var marker = new google.maps.Marker({
-//       position: austin,
-//       map: map
-// });
+
+
 
 
