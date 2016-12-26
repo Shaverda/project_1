26f2419aed7491ae3923b95f461529d9d4958013
Shaverda <shaverda@gmail.com>
2016-12-26 16:35:41 -0600

    fixed some stuff on scheduler--need to loop through different details

--- assets/javascript/secondpage.js
+++ assets/javascript/secondpage.js
@@ -14,7 +14,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var flight_options_ref = database.ref("flight_options");
 
-var destination_options = [	//hard-coded destination array of arrays w/ the city name and the airport code linked
+var destination_options = [ //hard-coded destination array of arrays w/ the city name and the airport code linked
     ["Atlanta, GA", "ATL"],
     ["Chicago, IL ", "ORD"],
     ["Los Angeles, CA", "LAX"],
@@ -25,7 +25,7 @@ var destination_options = [	//hard-coded destination array of arrays w/ the city
 ];
 
 var destination_number = Math.floor(Math.random() * (destination_options.length));
-	//finds a random destination number 
+//finds a random destination number 
 
 var flight_request = {
     "request": {
@@ -45,14 +45,14 @@ var flight_request = {
 
 var starting_airport, departure_date, email, name, home;
 
-flight_options_ref.on("value", function(snapshot) {	//pulls firebase values from database
+flight_options_ref.on("value", function(snapshot) { //pulls firebase values from database
     starting_airport = snapshot.val().airport;
     departure_date = snapshot.val().date;
     email = snapshot.val().emailAddress;
     name = snapshot.val().firstName;
     home = snapshot.val().home_city;
 
-    flight_request.request.slice[0].origin = starting_airport;	//replaces our flight request for QPX w/ firebase data values
+    flight_request.request.slice[0].origin = starting_airport; //replaces our flight request for QPX w/ firebase data values
     flight_request.request.slice[0].date = departure_date;
     flight_request.request.slice[0].destination = destination_options[destination_number][1];
 
@@ -74,14 +74,14 @@ flight_options_ref.on("value", function(snapshot) {	//pulls firebase values from
                 window.location.href = 'index.html';
             }
             if (!(data_obj.hasOwnProperty("city"))) { //Essentially checks to see if QPX returns no flights; object returned by QPX will have no .city property if no flights were found. Handler displays error headline for 5 seconds before redirecting to home page to re-try inputs.
-                setTimeout(homepage_returner, 10000);  
-                $("#map").hide();					  
+                setTimeout(homepage_returner, 10000);
+                $("#map").hide();
                 $("#heading_info").html("<h1 style='font-weight:500;'> Sorry. There are no flights from this location. You will be redirected to the home page, but really you should probably redirect your life. </h1>");
             }
             var destination_city = data_obj.city[1].name;
             var sales_price = data.trips.tripOption[0].saleTotal.substr(3);
-   			var flight_info = data_obj.carrier[0].name + " #" + data.trips.tripOption[0].slice[0].segment[0].flight.number;
-   			var departure_time = data.trips.tripOption[0].slice[0].segment[0].leg[0].departureTime.slice(11,16);
+            var flight_info = data_obj.carrier[0].name + " #" + data.trips.tripOption[0].slice[0].segment[0].flight.number;
+            var departure_time = data.trips.tripOption[0].slice[0].segment[0].leg[0].departureTime.slice(11, 16);
 
             $("#flight-price").html("Flight price: $" + sales_price);
             $("#flight-info").html("Flight information: " + flight_info);
@@ -98,7 +98,7 @@ flight_options_ref.on("value", function(snapshot) {	//pulls firebase values from
 
 });
 
-function initMap() {	//initializes map on page load, adding a marker to the chosen randomzied destination
+function initMap() { //initializes map on page load, adding a marker to the chosen randomzied destination
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: { lat: 40.054, lng: -98.866 }
@@ -127,27 +127,37 @@ function geocodeAddress(geocoder, resultsMap) {
 }
 
 
-function eventful_request(){
-   var formatted_eventful_date = moment(departure_date).format('YYYYMMDD00');
-   var future_eventful_search_date = moment(departure_date).add(7, 'days').calendar();
-   future_eventful_search_date = moment(future_eventful_search_date).format('YYYYMMDD00');
+function eventful_request() {
+    var formatted_eventful_date = moment(departure_date).format('YYYYMMDD00');
+    var future_eventful_search_date = moment(departure_date).add(7, 'days').calendar();
+    future_eventful_search_date = moment(future_eventful_search_date).format('YYYYMMDD00');
 
-   var formatted_eventful_dates = formatted_eventful_dates + "-" + future_eventful_search_date;
+    var formatted_eventful_dates = formatted_eventful_date + "-" + future_eventful_search_date;
 
-   var oArgs = {
-      app_key: "hbM7xmwCJxwk2hjn",
-      q: "music",
-      where: destination_options[destination_number][0], 
-      "date": formatted_eventful_dates,
-      sort_order: "popularity",
+    var oArgs = {
+        app_key: "hbM7xmwCJxwk2hjn",
+        q: "music",
+        where: destination_options[destination_number][0],
+        "date": formatted_eventful_dates,
+        sort_order: "popularity",
+    };
 
-   };
+    console.log(oArgs);
 
-
-   EVDB.API.call("/events/search", oArgs, function(data) {
+    EVDB.API.call("/events/search", oArgs, function(data) {
         console.log(JSON.stringify(data));
-   		console.log(data);	
+        console.log(data);
+        for (var i = 0; i < 5; i++) {
+        	var formatted_date = moment(data.events.event[i].start_time).format('MMMM DD YY');
+        	$(".event-month").html(formatted_date.slice(0,3));
+        	$(".event-day").html(formatted_date.slice(-5, -3));
+        	$(".event-desc-header").html(data.events.event[i].title);
+
+        	$(".rsvp").attr("href", "www.google.com"); 
+        	//data.events.event[i].url
+        }
     });
+
 
 }
 
